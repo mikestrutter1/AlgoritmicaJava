d9ee23e1114fdb21c4790df7c4e9165c4d4f703c
Mike Strutter <filipsonimnx@gmail.com>
2016-10-22 23:17:49 +0300

    String Reverse Alghoritm

--- src/com/fmi/Main.java
+++ src/com/fmi/Main.java
@@ -1,11 +1,27 @@
 package com.fmi;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("Hello World");
+        char[] text = "Ana are mere".toCharArray();
+        int start = 0, finish;
+        for (int i = 0; i < text.length; i++) {
+            if (text[i] == ' ' || i==text.length-1) {
+                finish=i-1;
+                while(start<finish)
+                {
+                    char aux = text[start];
+                    text[start]=text[finish];
+                    text[finish]=aux;
+                    start++;
+                    finish--;
+                }
+                start=i+1;
+            }
+        }
 
-
+        System.out.println(String.valueOf(text));
     }
 }
